c12d82a9ca46f1e70c96b8d92ac83c9be9163236
Bianca van Schaik <bvschaik@gmail.com>
2019-11-17 11:16:23 +0100

    Day 19 part 2

--- 2018/day19.py
+++ 2018/day19.py
@@ -1,4 +1,5 @@
 import re
+from functools import partial
 from runner import runner
 
 def exec_addr(a, b, c, registers):
@@ -49,6 +50,11 @@ def exec_eqri(a, b, c, registers):
 def exec_eqrr(a, b, c, registers):
     registers[c] = 1 if registers[a] == registers[b] else 0
 
+def exec_divisor(ip, p, q, r, registers):
+    if registers[p] % registers[q] == 0:
+        registers[r] += registers[q]
+    registers[ip] += 9
+
 instructions = {
     'addr': exec_addr,
     'addi': exec_addi,
@@ -68,19 +74,11 @@ instructions = {
     'eqrr': exec_eqrr
 }
 
-class sample:
-    def __init__(self):
-        self.before = None
-        self.after = None
-        self.instruction = None
-
-    def __repr__(self):
-        return str.format("%s => %s with %s" % (self.before, self.after, self.instruction))
-
 class day19(runner):
     def __init__(self):
         self.ip = 0
         self.program = []
+        self.optimization = None
 
     def day(self):
         return 19
@@ -93,15 +91,16 @@ class day19(runner):
             self.program.append([m.group(1), int(m.group(2)), int(m.group(3)), int(m.group(4))])
 
     def solve1(self):
+        self.optimize_divisor()
         count = 0
         ip_index = self.ip
         max_ip = len(self.program)
         registers = [0, 0, 0, 0, 0, 0]
         while registers[ip_index] >= 0 and registers[ip_index] < max_ip:
             (instr, a, b, c) = self.program[registers[ip_index]]
-            print('ip=%d %s %s %d %d %d ' % (registers[ip_index], registers, instr, a, b, c), end = '')
+            #print('ip=%d %s %s %d %d %d ' % (registers[ip_index], registers, instr, a, b, c), end = '')
             instructions[instr](a, b, c, registers)
-            print(registers)
+            #print(registers)
             registers[ip_index] += 1
             count += 1
 
@@ -109,21 +108,41 @@ class day19(runner):
         return str(registers[0])
 
     def solve2(self):
-        return
+        self.optimize_divisor()
         count = 0
         ip_index = self.ip
         max_ip = len(self.program)
         registers = [1, 0, 0, 0, 0, 0]
-        while registers[ip_index] >= 0 and registers[ip_index] < max_ip and count < 10000:
+        while registers[ip_index] >= 0 and registers[ip_index] < max_ip:
             (instr, a, b, c) = self.program[registers[ip_index]]
-            print('ip=%d %s %s %d %d %d ' % (registers[ip_index], registers, instr, a, b, c), end = '')
+            #print('ip=%d %s %s %d %d %d ' % (registers[ip_index], registers, instr, a, b, c), end = '')
             instructions[instr](a, b, c, registers)
-            print(registers)
+            #print(registers)
             registers[ip_index] += 1
             count += 1
 
         print(count)
         return str(registers[0])
+
+    def optimize_divisor(self):
+        for index in range(len(self.program)):
+            if self.is_divisor_instruction_set(index):
+                # This calcalates: given P and Q, if Q divides P (e.g. P % Q == 0), add Q to R
+                p_register = self.program[index + 2][2]
+                q_register = self.program[index + 1][1]
+                r_register = self.program[index + 5][3]
+                instructions['divisor'] = partial(exec_divisor, self.ip)
+                self.program[index] = ['divisor', p_register, q_register, r_register]
+                # print("Optimized divisor at index", index)
+
+    def is_divisor_instruction_set(self, index):
+        chain = ['seti', 'mulr', 'eqrr', 'addr', 'addi', 'addr', 'addi', 'gtrr', 'addr', 'seti']
+        if index + len(chain) > len(self.program):
+            return False
+        for delta, instr in enumerate(chain):
+            if self.program[index + delta][0] != instr:
+                return False
+        return True
 
 day19().test('Sample input', [
     '#ip 0',
