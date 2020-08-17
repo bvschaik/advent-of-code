f1aadff64be565917a91b32e4d42ee5378db03ee
Bianca van Schaik <bvschaik@gmail.com>
2020-08-17 19:51:25 +0200

    Use simpler branch-and-bound algorithm for 2018 day 23

--- 2018/day23.py
+++ 2018/day23.py
@@ -1,19 +1,75 @@
 import re
 from runner import runner
 
-class nanobot:
-    def __init__(self, x, y, z, radius):
+class position:
+    def __init__(self, x, y, z):
         self.x = x
         self.y = y
         self.z = z
-        self.radius = radius
-        self.neighbours = set()
 
-    def distance_to(self, n):
-        return abs(self.x - n.x) + abs(self.y - n.y) + abs(self.z - n.z)
+    def distance_to(self, other):
+        return abs(self.x - other.x) + abs(self.y - other.y) + abs(self.z - other.z)
 
-    def __repr__(self):
-        return str.format("nanobot(%d, %d, %d)" % (self.x, self.y, self.z))
+    def distance_to_origin(self):
+        return abs(self.x) + abs(self.y) + abs(self.z)
+
+    def translate(self, dx, dy, dz):
+        return position(self.x + dx, self.y + dy, self.z + dz)
+
+class nanobot:
+    def __init__(self, x, y, z, r):
+        self.pos = position(x, y, z)
+        self.r = r
+
+    def in_range(self, pos):
+        return self.pos.distance_to(pos) <= self.r
+
+class cube:
+    def __init__(self, center, r, bots):
+        self.center = center
+        self.min_pos = position(center.x - r, center.y - r, center.z - r)
+        self.max_pos = position(center.x + r - 1, center.y + r - 1, center.z + r - 1)
+        self.r = r
+        self.bots = list(filter(lambda b: self.intersects(b), bots))
+        self.bots_in_range = len(self.bots)
+
+    def intersects(self, bot):
+        if self.r == 0:
+            return bot.in_range(self.center)
+        # Calculate minimum distance required to travel along each axis to
+        # arrive at the bounding box. Sum to find the Manhattan distance.
+        distance = (
+            self.min_axis_dist(bot.pos.x, self.min_pos.x, self.max_pos.x) +
+            self.min_axis_dist(bot.pos.y, self.min_pos.y, self.max_pos.y) +
+            self.min_axis_dist(bot.pos.z, self.min_pos.z, self.max_pos.z))
+        return distance <= bot.r
+
+    def min_axis_dist(self, target, a_min, a_max):
+        if target < a_min:
+            return a_min - target
+        elif target > a_max:
+            return target - a_max
+        else:
+            return 0
+
+    def split(self):
+        if self.r == 0:
+            return []
+        new_r = self.r // 2
+        low = -new_r if new_r > 0 else -1
+        high = new_r
+        cubes = [
+            cube(self.center.translate(low, low, low), new_r, self.bots),
+            cube(self.center.translate(low, low, high), new_r, self.bots),
+            cube(self.center.translate(low, high, low), new_r, self.bots),
+            cube(self.center.translate(low, high, high), new_r, self.bots),
+            cube(self.center.translate(high, low, low), new_r, self.bots),
+            cube(self.center.translate(high, low, high), new_r, self.bots),
+            cube(self.center.translate(high, high, low), new_r, self.bots),
+            cube(self.center.translate(high, high, high), new_r, self.bots)
+        ]
+        cubes.sort(key = lambda c: c.bots_in_range, reverse = True)
+        return cubes
 
 class day23(runner):
     def __init__(self):
@@ -27,54 +83,34 @@ class day23(runner):
         self.bots.append(nanobot(int(m.group(1)), int(m.group(2)), int(m.group(3)), int(m.group(4))))
 
     def solve1(self):
-        max_bot = max(self.bots, key = lambda b: b.radius)
-        in_range = 0
-        for bot in self.bots:
-            if bot.distance_to(max_bot) <= max_bot.radius:
-                in_range += 1
+        max_bot = max(self.bots, key = lambda b: b.r)
+        in_range = sum(1 for b in self.bots if max_bot.in_range(b.pos))
         return str(in_range)
 
     def solve2(self):
-        # Find pairs of nodes that have at least one point which is in range of both nodes
-        for ia, a in enumerate(self.bots):
-            for ib in range(ia + 1, len(self.bots)):
-                b = self.bots[ib]
-                if a.distance_to(b) <= a.radius + b.radius and a is not b:
-                    a.neighbours.add(b)
-                    b.neighbours.add(a)
+        self.max_bots = 0
+        self.max_position = position(0, 0, 0)
+        start_cube = cube(position(0, 0, 0), 2**30, self.bots)
+        self.binary_search([start_cube])
+        return str(self.max_position.distance_to_origin())
 
-        # Assuming: A and B, A and C, and B and C are neighbours: there's a spot where we are in range of all 3
-        # Find largest 'clique' of inter-connected nodes
-        max_clique = self.bron_kerbosch(set(), set(self.bots), set())
+    def binary_search(self, cubes):
+        for c in cubes:
+            if c.bots_in_range > self.max_bots:
+                # print("Searching cube %d %d %d %d" % (c.center.x, c.center.y, c.center.z, c.r))
+                self.update_search_state(c)
+                self.binary_search(c.split())
 
-        # The overlapping region closest to the origin is the maximum distance to the origin
-        # of a node minus its radius: if there were a point farther away, then its distance from
-        # the origin would be larger
-        origin = nanobot(0, 0, 0, 0)
-        answer = max(map(lambda x: x.distance_to(origin) - x.radius, max_clique))
-
-        return str(answer)
-
-    def bron_kerbosch(self, r, p, x, max_size = 0):
-        if not p and not x:
-            # print(len(r), max_size)
-            return r
-        if len(p) + len(x) + len(r) <= max_size:
-            # short-circuit
-            return set()
-        pivot = max(p | x, key = lambda n: len(n.neighbours))
-        non_neighbours = p - pivot.neighbours
-        pp = set(p)
-        xx = set(x)
-        max_clique = set()
-        for v in non_neighbours:
-            clique = self.bron_kerbosch(r | {v}, pp & v.neighbours, xx & v.neighbours, len(max_clique))
-            if len(clique) > len(max_clique):
-                max_clique = clique
-            pp.remove(v)
-            xx.add(v)
-        return max_clique
-
+    def update_search_state(self, c):
+        if c.r != 0 or c.bots_in_range < self.max_bots:
+            return
+        if c.bots_in_range > self.max_bots:
+            # print("Updating min to %d" % c.bots_in_range)
+            self.max_bots = c.bots_in_range
+            self.max_position = c.center
+        elif c.center.distance_to_origin() < self.max_position.distance_to_origin():
+            self.max_position = c.center
+            
 day23().test('Sample input', [
     'pos=<0,0,0>, r=4',
     'pos=<1,0,0>, r=1',
